645845f266183bd3e0bd761281184d79c8fe62ce
Colin Runciman <colin@colinruimansmbp.home>
2019-08-06 17:08:05 +0100

    Reducing Laboratory imports and exports.

--- Laboratory.hs
+++ Laboratory.hs
@@ -1,25 +1,8 @@
 -- All the necessary equipment for tests and experiments.
-module Laboratory (
-                   module Data.List,
-                   module Data.Maybe,
-                   module Catalogue,
-                   module Comparison,
-                   module Expression,
-                   module Generator,
+module Laboratory (module LazySmallCheck,
                    module Metrics,
-                   module Pressing,
-                   module Properties,
-                   module Shrinking,
-                   module Stellation) where
+                   module Properties) where
 
-import Data.List
-import Data.Maybe
-import Catalogue
-import Comparison
-import Expression
-import Generator
+import LazySmallCheck
 import Metrics
-import Pressing hiding (lMostCom', lMostComList, rMostCom', rMostComList)
 import Properties
-import Shrinking
-import Stellation
