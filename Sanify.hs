83d6f315ebc04d38c8f867095361f29ddbdc7e6a
smk-kent <32130683+smk-kent@users.noreply.github.com>
2019-08-06 12:14:50 +0100

    Add files via upload
    very simple trafos to make expressions ready for comparison, also exports 'safe' comparisons '<<==' and '====' that sanify expressions first before comparisons. Sanifications leaves graded expressions alone.

---
+++ Sanify.hs
@@ -0,0 +1,63 @@
+module Sanify where
+
+-- very basic transformation, should be done to expressions before === if not katagraded
+-- this will add insects if none existed
+
+import Info
+import Expression
+import Comparison
+import List
+
+unGraded :: RE -> Bool
+unGraded (Alt i _) = null $ gr i
+unGraded (Cat i _) = null $ gr i
+unGraded (Rep e)   = isRep e || isOpt e || isLam e || isEmp e || ungraded e
+unGraded (Opt e)   = unGraded (Rep e)
+ungraded _         = False
+
+sanify :: RE -> RE
+sanify  e | unGraded e = homTrans sanHom e
+          |  otherwise = e
+
+sanHom :: HomTrans 
+sanHom = HomTrans { falt = sanAlt, fcat = sanCat, frep = sanRep, fopt = sanOpt }
+
+sanOpt :: RE -> RE
+sanOpt Emp     = Lam
+sanOpt Lam     = Lam
+sanOpt x       | ewp x
+               = x
+               | otherwise
+               = Opt x
+
+sanRep :: RE -> RE
+sanRep Emp = Lam
+sanRep Lam = Lam
+sanRep x   = Rep x
+
+sanCat xs = upgradeRE RepCxt NoGrade $ cat xs
+
+-- sanAlt throws out duplicates, as a+a===a would be an issue
+-- it really is only an issue for 'sub'
+sanAlt xs | any ewp xs
+          = sanOpt na
+          | otherwise
+          = na
+            where
+            na  = upgradeRE RepCxt NoGrade (mkAlt nxs)
+            nxs = unions $ map altList xs
+            altList Emp        = []
+            altList Lam        = []
+            altList (Alt _ xs) = xs
+            altList (Opt x)    = altList x
+            altList y          = [y]
+
+-- safe sublang, and lang
+(<<==) :: RE -> RE -> Bool
+x <<== y = sanify x `sub` sanify y
+
+(====) :: RE -> RE -> Bool
+x ==== y = sanify x === sanify y
+
+
+
