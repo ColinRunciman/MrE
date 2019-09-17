be11847b9f2d897936099cb06ac63eb688684a0d
smk-kent <32130683+smk-kent@users.noreply.github.com>
2019-09-17 12:07:53 +0100

    allReg for Windows
    part (i) of creating Windows versions of shell scripts

---
+++ allReg.cmd
@@ -0,0 +1,8 @@
+IF EXIST populations (del /Q populations\*) ELSE (mkdir populations)
+set sizes=(10,20,40,80,160,320,640,1280,2560)
+set asizes=(1,2,3,4,8,16)
+FOR %%i in %sizes% DO (
+    FOR %%j in %asizes% DO (
+    Reg.exe -s%%i -w%%j -q1000 > populations/s%%iw%%j
+))
+
