56cca8b0c7b71ba0b50e8cbec1a1a5f2955b3988
ColinRunciman <colin.runciman@york.ac.uk>
2020-05-15 12:12:51 +0100

    New arrivals in the archive directory.

---
+++ archive/RunTimesU.hs
@@ -0,0 +1,5 @@
+
+import ReadTable
+
+main = processDirectory "utimes" $
+       produceGenericTable "Average simplification time for unlimited trafos, for size:" "Run-times (ms) for expressions of size " 1000.0
