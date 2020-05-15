55dcf93d5b9a6bd0871e48339356369a0331d058
ColinRunciman <colin.runciman@york.ac.uk>
2020-05-15 12:09:22 +0100

    Archiving SWPtest.hs FactSearch.hs Excelation.hs Exhaustive.hs RunTimesU.hs as they are not part of the current toolset but could again be useful.

--- RunTimesU.hs
+++
@@ -1,5 +0,0 @@
-
-import ReadTable
-
-main = processDirectory "utimes" $
-       produceGenericTable "Average simplification time for unlimited trafos, for size:" "Run-times (ms) for expressions of size " 1000.0
